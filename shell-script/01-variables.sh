277a61ed05d78df030481210be798bf74fc1849e
venkatesh <venkateshvenky626@gmail.com>
2025-08-01 10:53:17 +0530

    practice

--- shell-script/01-variables.sh
+++ shell-script/01-variables.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
 
-PERSON1=raju
-PERSON2=ravi
-echo "
-
-$PERSON1 : hi $PERSON2, how are u
-$PERSON2 : i am fine $PERSON1, what about you
-$PERSON1 : i am good, $PERSON2 shall we go movie
-$PERSON2 : yeah $PERSON1, i will come. "
+echo "hai prends"
---
+++ shell-script/03-variables.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+DATE=$(date)
+
+echo " TODAY DATE = ${DATE} "
